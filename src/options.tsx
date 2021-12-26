9858af5f575043795e4294af2a41081b791aa4a2
Manas Khandelwal <mkdpsj@gmail.com>
2021-12-27 02:26:46 +0530

    initial commit

---
+++ src/options.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Options = () => {
+  return (
+    <>
+      <h1>Options</h1>
+    </>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/popup.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Popup = () => {
+  return (
+    <>
+      <h1>PopUp</h1>
+    </>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
